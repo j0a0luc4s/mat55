014616a2493420066b1e955475b7bf3885c67d8a
j0a0luc4s <joaolucasrrolim@gmail.com>
2024-04-01 23:49:10 -0300

    Add minor assert to activity 3 + delivery 3

--- delivery/MAT_55_Lista03_DouglasMassahiroKotsubo_JoaoLucasRochaRolim.jl
+++ delivery/MAT_55_Lista03_DouglasMassahiroKotsubo_JoaoLucasRochaRolim.jl
@@ -120,10 +120,12 @@ function cholesky_decomposition(A::Matrix{T}; atol::T = 1e-6) where {T <: Abstra
 
     G = zeros(n, n)
 
+    @assert !isapprox(A[1, 1], 0; atol=atol) "Null Pivot"
     G[:, 1] = A[:, 1] / sqrt(A[1, 1])
     for i = 2:n
         G[i:n, i] = A[i:n, i]
         G[i:n, i] = G[i:n, i] - G[i:n, 1:(i-1)]*G[i, 1:(i-1)]
+        @assert !isapprox(G[i, i], 0; atol=atol) "Null Pivot"
         G[i:n, i] = G[i:n, i] / sqrt(G[i, i])
     end
 
