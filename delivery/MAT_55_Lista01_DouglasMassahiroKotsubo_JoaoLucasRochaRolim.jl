5c9e1879fe8167765d22b62b9f03808da4716a24
j0a0luc4s <joaolucasrrolim@gmail.com>
2024-04-01 21:34:41 -0300

    Change identation default

--- delivery/MAT_55_Lista01_DouglasMassahiroKotsubo_JoaoLucasRochaRolim.jl
+++ delivery/MAT_55_Lista01_DouglasMassahiroKotsubo_JoaoLucasRochaRolim.jl
@@ -1,59 +1,59 @@
 using LinearAlgebra
 
 function upper_direct_substitution(A::Matrix{T}, b::Vector{T}; atol::T=1e-6) where {T<:AbstractFloat}
-        @assert size(A, 1) == size(A, 2) == length(b) "A and b dimension mismatch"
-        n = size(A, 1)
+    @assert size(A, 1) == size(A, 2) == length(b) "A and b dimension mismatch"
+    n = size(A, 1)
 
-        @assert all(all(isapprox.(A[i, 1:(i-1)], 0, atol=atol)) for i = 1:n) "A must be an upper triangular matrix"
-        @assert all(!isapprox(A[i, i], 0, atol=atol) for i = 1:n) "A must be a non-singular matrix"
+    @assert all(all(isapprox.(A[i, 1:(i-1)], 0, atol=atol)) for i = 1:n) "A must be an upper triangular matrix"
+    @assert all(!isapprox(A[i, i], 0, atol=atol) for i = 1:n) "A must be a non-singular matrix"
 
-        c = zeros(length(b))
+    c = zeros(length(b))
 
-        for i = n:-1:1
-                c[i] = (b[i] - A[i, :]' * c) / A[i, i]
-        end
+    for i = n:-1:1
+        c[i] = (b[i] - A[i, :]' * c) / A[i, i]
+    end
 
-        return c
+    return c
 end
 
 function lower_direct_substitution(A::Matrix{T}, b::Vector{T}; atol::T=1e-6) where {T<:AbstractFloat}
-        @assert size(A, 1) == size(A, 2) == length(b) "A and b dimension mismatch"
-        n = size(A, 1)
+    @assert size(A, 1) == size(A, 2) == length(b) "A and b dimension mismatch"
+    n = size(A, 1)
 
-        @assert all(all(isapprox.(A[i, (i+1):n], 0, atol=atol)) for i = 1:n) "A must be a lower triangular matrix"
-        @assert all(!isapprox(A[i, i], 0, atol=atol) for i = 1:n) "A must be a non-singular matrix"
+    @assert all(all(isapprox.(A[i, (i+1):n], 0, atol=atol)) for i = 1:n) "A must be a lower triangular matrix"
+    @assert all(!isapprox(A[i, i], 0, atol=atol) for i = 1:n) "A must be a non-singular matrix"
 
-        c = zeros(length(b))
+    c = zeros(length(b))
 
-        for i = 1:n
-                c[i] = (b[i] - A[i, :]' * c) / A[i, i]
-        end
+    for i = 1:n
+        c[i] = (b[i] - A[i, :]' * c) / A[i, i]
+    end
 
-        return c
+    return c
 end
 
 function gaussian_elimination(A::Matrix{T}, b::Vector{T}; atol::T=1e-6) where {T<:AbstractFloat}
-        @assert size(A, 1) == size(A, 2) == length(b) "A and b dimension mismatch"
-        n = size(A, 1)
+    @assert size(A, 1) == size(A, 2) == length(b) "A and b dimension mismatch"
+    n = size(A, 1)
 
-        @assert !isapprox(det(A), 0; atol=atol) "A must be a non-singular matrix"
+    @assert !isapprox(det(A), 0; atol=atol) "A must be a non-singular matrix"
 
-        _A = deepcopy(A)
-        _b = deepcopy(b)
+    _A = deepcopy(A)
+    _b = deepcopy(b)
 
-        for k = 1:(n-1)
-                τ = vcat(zeros(k), _A[(k+1):n, k] / _A[k, k])
-                e = vcat(zeros(k - 1), 1, zeros(n - k))
+    for k = 1:(n-1)
+        τ = vcat(zeros(k), _A[(k+1):n, k] / _A[k, k])
+        e = vcat(zeros(k - 1), 1, zeros(n - k))
 
-                M = I - τ * e'
+        M = I - τ * e'
 
-                _A = M * _A
-                _b = M * _b
-        end
+        _A = M * _A
+        _b = M * _b
+    end
 
-        c = upper_direct_substitution(_A, _b)
+    c = upper_direct_substitution(_A, _b)
 
-        return c
+    return c
 end
 
 println("Type in the number of variables: ")
@@ -61,7 +61,7 @@ n = parse(Int64, readline())
 println("Type in the square matrix A (", n, " lines with ", n, " numbers each): ")
 A = Matrix{Float64}(undef, n, n)
 for i = 1:n
-        A[i, :] = Float64[parse(Float64, el) for el in split(readline())]
+    A[i, :] = Float64[parse(Float64, el) for el in split(readline())]
 end
 println("Type in the vector b (1 line with ", n, " numbers): ")
 b = Vector{Float64}(undef, n)
@@ -72,14 +72,14 @@ println("2. lower_direct_substitution")
 println("3. gaussian_elimination")
 method = parse(Int64, readline())
 if method == 1
-        c = upper_direct_substitution(A, b)
-        println("solution = ", c)
+    c = upper_direct_substitution(A, b)
+    println("solution = ", c)
 elseif method == 2
-        c = lower_direct_substitution(A, b)
-        println("solution = ", c)
+    c = lower_direct_substitution(A, b)
+    println("solution = ", c)
 elseif method == 3
-        c = gaussian_elimination(A, b)
-        println("solution = ", c)
+    c = gaussian_elimination(A, b)
+    println("solution = ", c)
 else
-        println("invalid method")
+    println("invalid method")
 end
--- src/2/lu_decomposition_pivot.jl
+++ src/2/lu_decomposition_pivot.jl
@@ -1,29 +1,29 @@
 function lu_decomposition_pivot(A::Matrix{T}; atol::T=1e-6) where {T<:AbstractFloat}
-        @assert size(A, 1) == size(A, 2) "A must be a square matrix"
-        n = size(A, 1)
+    @assert size(A, 1) == size(A, 2) "A must be a square matrix"
+    n = size(A, 1)
 
-        L = Matrix{T}(I, n, n)
-        U = deepcopy(A)
-        P = Matrix{T}(I, n, n)
+    L = Matrix{T}(I, n, n)
+    U = deepcopy(A)
+    P = Matrix{T}(I, n, n)
 
-        for k = 1:(n-1)
-                _, i = findmax(abs.(U[k:n, k]))
-                i = i + k - 1
-                @assert !isapprox(U[i, k], 0; atol=atol) "A must be a non-singular matrix"
+    for k = 1:(n-1)
+        _, i = findmax(abs.(U[k:n, k]))
+        i = i + k - 1
+        @assert !isapprox(U[i, k], 0; atol=atol) "A must be a non-singular matrix"
 
-                L[i, 1:(k-1)], L[k, 1:(k-1)] = L[k, 1:(k-1)], L[i, 1:(k-1)]
-                U[i, :], U[k, :] = U[k, :], U[i, :]
-                P[i, :], P[k, :] = P[k, :], P[i, :]
+        L[i, 1:(k-1)], L[k, 1:(k-1)] = L[k, 1:(k-1)], L[i, 1:(k-1)]
+        U[i, :], U[k, :] = U[k, :], U[i, :]
+        P[i, :], P[k, :] = P[k, :], P[i, :]
 
 
-                τ = vcat(zeros(k), U[(k+1):n, k] / U[k, k])
-                e = vcat(zeros(k - 1), 1, zeros(n - k))
+        τ = vcat(zeros(k), U[(k+1):n, k] / U[k, k])
+        e = vcat(zeros(k - 1), 1, zeros(n - k))
 
-                M = I - τ * e'
+        M = I - τ * e'
 
-                U = M * U
-                L = L - M + I
-        end
+        U = M * U
+        L = L - M + I
+    end
 
-        return L, U, P
+    return L, U, P
 end
